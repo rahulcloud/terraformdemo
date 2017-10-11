4ab89b6f4d6b6ff6a76bf6e2e0df2c549d3a949e
rahulcloud <drahulgandhi@gmail.com>
2017-10-11 10:55:41 +0000

    initial commit

---
+++ access.tf
@@ -0,0 +1,3 @@
+
+variable "aws_access_key" { default = "AKIAI4SXYAL42LEJXV3A" }
+variable "aws_secret_key" { default = "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" }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+
+variable "security_group" { default = "sg-192f9464" }
+
+variable "key_name" { default = "clouddevops" }
+variable "public_key" { default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2EZd4ma6wbrskoyXlQFLtUtQwh4LZy3eRniXykPkHy0rXPBbi57tsnQvUxgibXsmdYjAtqB+8xgfRjHxFBBixjIWEnoBxvxcufpeWSuLl5gyk1ISoN7SxV6qTlSrc6jieRKt4b1VNFp8uT4hwyeMZ1qQhGmPAnCu9biscUrECVppBFX+xLAxpMBM2Vz0nFJV8ZvG4uPc4w1njbW5QW6OVHH2sEuPYFvRcfcSgj1wKwkbR6VNqhJDEe4/CICqgLHzJgD3jSjBANVxX+/MhSdOhbnL2zmY4BDO7zeERfUOaiFmhnk0yU/b8rt9QMIjjzRMIu64a2iGYOU9AAb2pGB7h root@ip-11-0-0-196.eu-central-1.compute.internal" }
