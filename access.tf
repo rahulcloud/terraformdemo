d2a8b10cdba9dca1c2c488c02c48d41ea5fa1dd8
rahulcloud <drahulgandhi@gmail.com>
2017-10-17 09:11:09 +0000

    hgfjhgjh

--- access.tf
+++ access.tf
@@ -1,3 +1,3 @@
-variable "aws_access_key" { default = "AKIAJH7JCYU3QHUE763Q" }
-variable "aws_secret_key" { default = "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" }
+variable "aws_access_key" { default = "AKIAIEW5G2VQULRQZKMQ" }
+variable "aws_secret_key" { default = "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" }
 
