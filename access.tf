005ff06435d3b9eecdd92c09c78e79daed07f999
rahulcloud <drahulgandhi@gmail.com>
2017-10-18 10:39:20 +0000

    changes made

--- access.tf
+++ access.tf
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-
-
-=======
->>>>>>> ea3d432707db8ad5f23dc3874bfee86cc6a9eb46
 variable "aws_access_key" { default = "AKIAIAEVNCRMRFJ2RRMQ" }
 variable "aws_secret_key" { default = "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" }
 
